1f0138a3be56eb92e3147ff2a5c72921f0eefdb9
Sjchew90 <116099313+Sjchew90@users.noreply.github.com>
2024-11-15 19:32:58 +0800

    Create Button_LED1-1.cpp

---
+++ Wokwi/Button_LED1-1.cpp
@@ -0,0 +1,17 @@
+// Push Button and Turn on LED
+const int LEDR_PIN = 23;
+const int BUTTONR_PIN = 25;
+
+void setup() {
+  pinMode(LEDR_PIN, OUTPUT);
+  pinMode(BUTTONR_PIN, INPUT_PULLUP);
+}
+
+void loop(){
+  if (digitalRead(BUTTONR_PIN) == LOW){
+    digitalWrite(LEDR_PIN,HIGH);
+  } 
+  else {
+    digitalWrite(LEDR_PIN,LOW);
+  }
+}
