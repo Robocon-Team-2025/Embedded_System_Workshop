3573f1a68d055da4c26426abb56380cfab44e0c3
Sjchew90 <116099313+Sjchew90@users.noreply.github.com>
2024-11-16 06:06:24 +0800

    Update Potentiometer_LED2-1.cpp

--- Wokwi/Potentiometer_LED2-1.cpp
+++ Wokwi/Potentiometer_LED2-1.cpp
@@ -4,26 +4,26 @@
 const int LEDR_PIN  = 23;
 const int LEDG_PIN  = 22;
 const int LEDB_PIN  = 21;
-const int POT_PIN  = 13;
-
-// Variable for Chaser
-bool light = false;
+const int POT1_PIN  = 13;
+const int POT2_PIN  = 12;
 
 void setup() {
   pinMode(LEDR_PIN, OUTPUT); // Setup pin 23 as Output pin
   pinMode(LEDG_PIN, OUTPUT); // Setup pin 22 as Output pin
   pinMode(LEDB_PIN, OUTPUT); // Setup pin 21 as Output pin
-  pinMode(POT_PIN, INPUT); // Setup pin 13 as Input pin
+  pinMode(POT1_PIN, INPUT); // Setup pin 13 as Input pin
+  pinMode(POT2_PIN, INPUT); // Setup pin 12 as Input pin
 }
 
 void loop() {
   int wait = analogRead(POT_PIN);
+  int light = analogRead(POT_PIN);
   wait = map(wait,0,4095,100,1000); // Delay can be change from 100 to 1000
-  light = !light;
-  digitalWrite(LEDR_PIN,light);
+  light = map(wait,0,4095,0,255); // Brightness can be change from 0 to 255
+  analogWrite(LEDR_PIN,light);
   delay(wait);
-  digitalWrite(LEDG_PIN,light);
+  analogWrite(LEDG_PIN,light);
   delay(wait);
-  digitalWrite(LEDB_PIN,light);
+  analogWrite(LEDB_PIN,light);
   delay(wait); 
 }
